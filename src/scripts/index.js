61d70ec1a4d4b8ff6d5579f5f03e86259514d07e
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-21 21:11:43 -0400

    added light style to toggle button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,32 +2,14 @@ const toggleBtn = document.querySelector("#toggle");
 const body = document.querySelector("body");
 const topBar = document.querySelector(".dash__header");
 const cards = document.querySelectorAll(".card");
-
-// toggleBtn.addEventListener("click", () => {
-//     if (toggleBtn.checked) {
-//         body.classList.add("light");
-//         topBar.classList.add("light");
-
-//         cards.forEach(card => {
-//             card.classList.add("light");
-//         });
-//     } else {
-//         body.classList.remove("light");
-//         topBar.classList.remove("light");
-
-//         cards.forEach(card => {
-//             card.classList.remove("light");
-//         });
-//     }
-// });
+const toggleSlider = document.querySelector(".toggle__slider");
 
 toggleBtn.addEventListener("click", () => {
-
     body.classList.toggle("light");
     topBar.classList.toggle("light");
+    toggleSlider.classList.toggle("light");
 
     cards.forEach(card => {
         card.classList.toggle("light");
     });
-
 });
