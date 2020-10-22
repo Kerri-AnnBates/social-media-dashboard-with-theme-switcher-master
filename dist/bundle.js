87efd264f9c08135e76dfc75cb1e13bbc96026e7
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2020-10-21 20:57:40 -0400

    toggle button functional

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,1 @@
-alert("connected");
+(()=>{var e=document.querySelector("#toggle"),t=document.querySelector("body"),c=document.querySelector(".dash__header"),l=document.querySelectorAll(".card");e.addEventListener("click",(function(){t.classList.toggle("light"),c.classList.toggle("light"),l.forEach((function(e){e.classList.toggle("light")}))}))})();
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,33 @@
-alert("connected");
+const toggleBtn = document.querySelector("#toggle");
+const body = document.querySelector("body");
+const topBar = document.querySelector(".dash__header");
+const cards = document.querySelectorAll(".card");
+
+// toggleBtn.addEventListener("click", () => {
+//     if (toggleBtn.checked) {
+//         body.classList.add("light");
+//         topBar.classList.add("light");
+
+//         cards.forEach(card => {
+//             card.classList.add("light");
+//         });
+//     } else {
+//         body.classList.remove("light");
+//         topBar.classList.remove("light");
+
+//         cards.forEach(card => {
+//             card.classList.remove("light");
+//         });
+//     }
+// });
+
+toggleBtn.addEventListener("click", () => {
+
+    body.classList.toggle("light");
+    topBar.classList.toggle("light");
+
+    cards.forEach(card => {
+        card.classList.toggle("light");
+    });
+
+});
